9666b5a16c9a0868ae04ca23af05e44a8d18a979
Maria Luisa <Maluworkq@gmail.com>
2026-03-12 22:35:45 -0300

    Aulas do dia 12/03
    --Php no Nginx

---
+++ AulaPwiii/aula1203/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Meu HTML</h1>
+    <?php 
+        $nome="besukk";
+
+        echo "<h2>Olá $nome, tenha uma boa noite !!!</h2>" 
+    ?>
+</body>
+</html>
